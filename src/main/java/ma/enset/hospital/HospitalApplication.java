7af141e3b54686ef53b74f5c258445d39a435c04
Bayi-Mostapha <bayimostapha@gmail.com>
2026-03-20 11:49:05 +0000

    repositories + mock data (patients + medecins)

--- src/main/java/ma/enset/hospital/HospitalApplication.java
+++ src/main/java/ma/enset/hospital/HospitalApplication.java
@@ -1,7 +1,15 @@
 package ma.enset.hospital;
 
+import org.apache.el.stream.Stream;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import ma.enset.hospital.entities.Medecin;
+import ma.enset.hospital.entities.Patient;
+import ma.enset.hospital.repositories.MedecinRepository;
+import ma.enset.hospital.repositories.PatientRepository;
 
 @SpringBootApplication
 public class HospitalApplication {
@@ -10,4 +18,23 @@ public class HospitalApplication {
 		SpringApplication.run(HospitalApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner start(PatientRepository patientRepository, MedecinRepository medecinRepository) {
+		return args -> {
+			java.util.stream.Stream.of("Hassan", "Yassine", "Adnane").forEach(name -> {
+				Patient patient = new Patient();
+				patient.setNom(name);
+				patient.setDateNaissance(new java.sql.Date(System.currentTimeMillis()));
+				patient.setMalade(false);
+				patientRepository.save(patient);
+			});
+			java.util.stream.Stream.of("Aymane", "Haytham", "Mostapha").forEach(name -> {
+				Medecin medecin = new Medecin();
+				medecin.setNom(name);
+				medecin.setEmail(name + "@gmail.com");
+				medecin.setSpecialite(Math.random() > 0.5 ? "Cardio" : "Dentiste");
+				medecinRepository.save(medecin);
+			});
+		};
+	};
 }
